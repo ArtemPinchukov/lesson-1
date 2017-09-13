bdd0427435987dae39a71a3740a5fc84e676bb4f
ArtemPinchukov <artem-artik@tut.by>
2017-09-13 20:25:48 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[]args) {
+        int a;//объявление переменной (выделение памяти)
+        a = 5;//инициализация переменной
+        System.out.println(a);
+        int b=10;//определение переменной
+        int sum=a+b;//сумма
+        System.out.println(sum);
+        int div=a/b;
+        System.out.println(div);/*вывод на экран*/
+        float f=5.6f;
+        a++;//прибавляет 1 к числу а
+        System.out.println(a);
+        a--;//отнимает 1 от a
+        System.out.println(a);
+    }
+
+
+}
